685fae0cf76000ada54bea42887befce455734f7
Brian Salehi <salehibrian@gmail.com>
2023-11-13 21:56:10 +0330

    new grammar-practice

---
+++ data/grammars/B1.1B.9.d
@@ -0,0 +1,8 @@
+Kritische Fragen
+
+You can reply to someone's informative sentence with a
+criticizing question.
+
+The "Kritische Fragen" sentence forms like this:
+
+«Was? <Verb> <Subject> wirklich, <Sätze> zu <Verb2>?»
